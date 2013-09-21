170e0bb25887d5964fff12254dac6634c5fd423d
kjsm <kenji33h@gmail.com>
2013-09-21 15:55:26 +0900

    Rewrite generators to template base

--- lib/generators/i18n_yaml/scaffold_controller/scaffold_controller_generator.rb
+++ lib/generators/i18n_yaml/scaffold_controller/scaffold_controller_generator.rb
@@ -2,48 +2,35 @@ module I18nYaml
   module Generators #:nodoc:
     class ScaffoldControllerGenerator < Rails::Generators::NamedBase #:nodoc:
       include Rails::Generators::ResourceHelpers
-      include I18nYamlGenerator::Helpers
-      class_option :commented, :default => Rails.application.config.i18n_yaml_generator.commented
+
+      source_root File.expand_path("../templates", __FILE__)
+
+      attr_reader :locale
 
       def views_i18n_yaml_file
-        titles_hash = {
-          'index'  => { 'title' => "#{human_name.pluralize}" },
-          'show'   => { 'title' => human_name },
-          'edit'   => { 'title' => "Editing #{human_name}" },
-          'new'    => { 'title' => "New #{human_name}" }
-        }
-
-        flash_hash = {
-          'create' => {
-            'notice' => '%{resource_name} was successfully created.'
-          },
-          'update' => {
-            'notice' => '%{resource_name} was successfully updated.'
-          },
-          'destroy' => {
-            'notice' => '%{resource_name} was successfully deleted.',
-            'alert' => '%{resource_name} could not be deleted.'
-          }
-        }
-
-        scope_array = controller_i18n_scope.split('.')
-        
         I18n.available_locales.each do |locale|
-          hash_for_yaml = {}
-          
-          hash_for_yaml.deep_merge! wrap_hash(titles_hash, scope_array)
-          hash_for_yaml.deep_merge! wrap_hash(flash_hash, %w(flash) + scope_array)
-
-          yaml_content = { locale.to_s => hash_for_yaml }.to_yaml
-
-          destination_path = File.join('config/locales/views/', "#{controller_file_path}.#{locale}.yml")
-          create_file destination_path, options.commented ? comment_yaml_body(yaml_content) : yaml_content
+          @locale = locale
+          template "i18n.yml", File.join("config/locales/views", controller_file_path, "#{locale}.yml")
         end
       end
 
+      protected
+
+      def i18n_namespacing(&block)
+        content = capture(&block)
+        content = wrap_with_i18n_namespace(content)
+        concat(content)
+      end
+
+      def wrap_with_i18n_namespace(content)
+        scopes = controller_i18n_scope.split('.')
+
+        namespaces = ''
+        scopes.each_with_index do |scope, index|
+          namespaces += indent("#{scope}:\n", (index + 1) * 2)
+        end
+        namespaces += indent(content, (scopes.size - 1) * 2)
+      end
     end
   end
 end
-
-
-
--- lib/i18n_yaml_generator.rb
+++ lib/i18n_yaml_generator.rb
@@ -1,22 +1,21 @@
 module I18nYamlGenerator
-  Configuration = Struct.new(:commented)
-
   class Railtie < ::Rails::Railtie
-    config.i18n_yaml_generator = I18nYamlGenerator::Configuration.new(false)
-
     generators do |app|
       if ::Rails::VERSION::STRING >= '3.2'
         Rails::Generators.configure!(app.config.generators)
       end
 
-      require 'i18n_yaml_generator/helpers'
-      require 'rails/generators/rails/scaffold_controller/scaffold_controller_generator'
       require 'rails/generators/rails/model/model_generator'
-      Rails::Generators::ModelGenerator.send(:hook_for, :i18n_yaml)
-      Rails::Generators::ModelGenerator.send(:class_option, :i18n_yaml, :default => 'i18n_yaml')
-      Rails::Generators::ScaffoldControllerGenerator.send(:hook_for, :i18n_yaml)
-      Rails::Generators::ScaffoldControllerGenerator.send(:class_option, :i18n_yaml, :default => 'i18n_yaml')
+      Rails::Generators::ModelGenerator.class_eval do
+        class_option :i18n_yaml, :default => 'i18n_yaml'
+        hook_for :i18n_yaml
+      end
+
+      require 'rails/generators/rails/scaffold_controller/scaffold_controller_generator'
+      Rails::Generators::ScaffoldControllerGenerator.class_eval do
+        class_option :i18n_yaml, :default => 'i18n_yaml'
+        hook_for :i18n_yaml
+      end
     end
   end
 end
-
